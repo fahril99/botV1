7a811dd0becfedb7d40530c20d30d86c053384c9
fahril99 <91009732+fahril99@users.noreply.github.com>
2022-03-01 14:14:44 +0700

    Update listjadibot.js

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -1,6 +1,6 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply(users.map(v => '🌟listbot utama🌟\nwa.me/6281914783843?text=.menu\n\n\jadibot=\nwa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+  m.reply(`🌟 *listBot Utama* 🌟\n https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🤖JadiBot* \n` + users.map(v => ' https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
 }
 handler.command = handler.help = ['listjadibot', 'ljb']
 handler.tags = ['jadibot', 'ljb']
