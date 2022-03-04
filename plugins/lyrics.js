b7b3380e08dec497fcc8191a4e59a4f167f6d04c
fahril99 <91009732+fahril99@users.noreply.github.com>
2022-03-04 14:28:02 +0700

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -2,23 +2,22 @@
 
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-m.reply(wait)
+  if (!text) throw 'Missing \'title\' query'
   let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', {
     title: text
   }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.thumbnail.genius) throw json
-  conn.sendFile(m.chat, json.thumbnail.genius, '', `
+  if (json.error) throw json.error
+  m.reply(`
 *${json.title}*
-_${json.author}_\n
-${json.lyrics}\n\n
-${json.links.genius}
-`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+_${json.author}_
 
+${json.lyrics}
+`.trim())
 }
 handler.help = ['lirik'].map(v => v + ' <Apa>')
-handler.tags = ['internet', 'edukasi']
+handler.tags = ['internet']
 handler.command = /^(lirik|lyrics|lyric)$/i
 
 module.exports = handler
