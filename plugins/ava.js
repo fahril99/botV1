53bab52b2ac300ab70fd458e42751f9344cb631a
fahril99 <91009732+fahril99@users.noreply.github.com>
2022-03-04 20:36:03 +0700

    Update ava.js

--- plugins/ava.js
+++ plugins/ava.js
@@ -1,25 +1,5 @@
 let fetch = require('node-fetch')
-
-let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'script')).buffer(), `
-
-*⚠SCRIPT BOT⚠*
-
-link script bot√
-
-https://youtu.be/9oe4BUWQbew
-
-(enc 3 file) 
-
-buat yang mau no enc√
-
-beli ke nomor wa 6285876902820
-
-harga diskon 20k
-
-30k ? udah di run kan di heroku make cc (credit card) 
-
-`.trim(), footer, let fetch = require('node-fetch')
-let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'script')).buffer(), `
+let handler = async (m, { conn }) => await conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'script')).buffer(), `
 *⚠SCRIPT BOT⚠*
 
 
@@ -33,17 +13,9 @@ beli ke nomor wa 6285876902820
 harga diskon 20k
 30k ? udah di run kan di heroku make cc (credit card) 
 
-`.trim(), footer, 'owner', '.owner', m)
+`.trim(), footer, 'owner', '.owner', 'thanks', '.tqto', 'menu', '.menu', m)
 handler.help = ['sc']
 handler.tags = ['info']
-handler.command = /^(sc|scbot|script)$/i
-
-module.exports = handler'owner', '.owner', m)
-
-handler.help = ['sc']
-
-handler.tags = ['info']
-
 handler.command = /^(sc|scbot|script)$/i
 
 module.exports = handler
